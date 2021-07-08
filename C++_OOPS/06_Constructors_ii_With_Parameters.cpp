e8be0acdbc12439d8a1f4a2e05ca586d7a4e8618
Dhiman_aman <72404186+AmanDhimanD@users.noreply.github.com>
2021-07-09 00:03:35 +0530

    Add files via upload

---
+++ C++_OOPS/06_Constructors_ii_With_Parameters.cpp
@@ -0,0 +1,29 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+class MyClass
+{
+public:
+    int num1;
+    int num2;
+    //Constructor with parameter
+    MyClass(int n1, int n2)
+    {
+        cout << "Costructor With parameter." << endl;
+        num1 = n1;
+        num2 = n2;
+        cout << "Value of n1 and n2 are " << n1 << " And " << n2 << endl;
+    }
+};
+int main()
+{
+    MyClass obj(12, 23); //auutomatic call the constructor
+
+    // print the value of class
+    cout<<"Class Attributes."<<endl;
+    cout << obj.num1 << endl;
+    cout << obj.num2 << endl;
+
+    return 0;
+}
