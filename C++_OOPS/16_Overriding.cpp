d089317a57a2ea3e497ce63c7db80396d43042e4
Dhiman_aman <72404186+AmanDhimanD@users.noreply.github.com>
2021-07-09 00:05:16 +0530

    Add files via upload

---
+++ C++_OOPS/16_Overriding.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Animals
+{
+public:
+    void eat()
+    {
+        cout << "Eating....." << endl;
+    }
+};
+class horse : public Animals
+{
+public:
+    void eat()
+    {
+        cout << "Eating Grass." << endl;
+    }
+};
+int main()
+{
+    // Animals h = horse();
+    horse h = horse();
+    h.eat();
+    return 0;
+}
