8648a1f13453e8cd0f25829673bd269898500134
Dhiman_aman <72404186+AmanDhimanD@users.noreply.github.com>
2021-07-14 20:18:12 +0530

    Add files via upload

---
+++ Conversions/Bitwise_Operator.cpp
@@ -0,0 +1,19 @@
+//#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+    int n = 4;
+    /* 
+    4 = 0100 --> after shift right
+        1000 --> binary of 2 
+    
+    4 = 0100 --> after shift right
+        0010 --> binary of 8 
+
+    */
+    cout << (n << 1)<<endl; //Right shift
+    cout << (n >> 1)<<endl; //Left shift
+    return 0;
+}
