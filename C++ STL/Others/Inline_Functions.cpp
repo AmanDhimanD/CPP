99bdfde571e6445e6f76b4195fcda82a57a073b4
Dhiman_aman <72404186+AmanDhimanD@users.noreply.github.com>
2021-07-20 23:50:40 +0530

    Add files via upload

---
+++ C++ STL/Others/Inline_Functions.cpp
@@ -0,0 +1,25 @@
+//#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+// Inline functions behave like macros.When an inline function gets called, instead of transferring the control to the function, the call gets substituted with the function code.Thus this saves time and improves performance.
+
+inline int Sum(int a, int b)
+{
+    return a + b;
+}
+int main()
+{
+    cout<<"Sum of Two Numbers are:- "<<Sum(2,5)<<endl;
+    cout<<"Sum of Two Numbers are:- "<<Sum(2,5)<<endl;
+    cout<<"Sum of Two Numbers are:- "<<Sum(2,5)<<endl;
+    cout<<"Sum of Two Numbers are:- "<<Sum(2,5)<<endl;
+    cout<<"Sum of Two Numbers are:- "<<Sum(2,5)<<endl;
+    cout<<"Sum of Two Numbers are:- "<<Sum(2,5)<<endl;
+    cout<<"Sum of Two Numbers are:- "<<Sum(2,5)<<endl;
+    cout<<"Sum of Two Numbers are:- "<<Sum(2,5)<<endl;
+    cout<<"Sum of Two Numbers are:- "<<Sum(2,5)<<endl;
+    cout<<"Sum of Two Numbers are:- "<<Sum(2,5)<<endl;
+    cout<<"Sum of Two Numbers are:- "<<Sum(2,5)<<endl;
+    return 0;
+}
