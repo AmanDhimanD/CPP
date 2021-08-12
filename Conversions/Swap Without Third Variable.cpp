ab83ea31041de52b8ce6743bab6573ef4db72a85
Dhiman_aman <72404186+AmanDhimanD@users.noreply.github.com>
2021-08-12 20:37:01 +0530

    Create Swap Without Third Variable.cpp

---
+++ Conversions/Swap Without Third Variable.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a=4,b=5;
+    cout<<a<<" "<<b<<endl;
+    a=a^b;
+    b=a^b;
+    a=a^b;
+    cout<<a<<" "<<b<<endl;
+    return 0;
+}
