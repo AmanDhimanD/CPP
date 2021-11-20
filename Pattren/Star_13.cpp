cf6da0e19a37e3058dd38b94e7f04333c1d55af3
Dhiman_aman <72404186+AmanDhimanD@users.noreply.github.com>
2021-11-20 21:03:40 +0530

    Add files via upload

---
+++ Pattren/Star_13.cpp
@@ -0,0 +1,25 @@
+//#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = n; i>=1; i--)
+    {
+        for (int j = n; j >= 1; j--)
+        {
+            if (i >= j)
+            {
+                cout << "* ";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
